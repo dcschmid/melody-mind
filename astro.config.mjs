f234a7e88820e391db3b6ae70aec24bd13c4d748
dcschmid <daniel.schmid1980@icloud.com>
2024-07-26 21:13:15 +0200

    feat: add gamehome page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [icon()]
 });
