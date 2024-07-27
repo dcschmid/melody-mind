20113a7f79218545f372b94c05b234897dee4c07
dcschmid <daniel.schmid1980@icloud.com>
2024-07-27 21:39:05 +0200

    feat: add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 import icon from "astro-icon";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [icon()]
+  integrations: [icon(), playformCompress()]
 });
