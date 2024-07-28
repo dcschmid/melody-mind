399923af84975e75458e99211f3eb72192ac28bf
dcschmid <daniel.schmid1980@icloud.com>
2024-07-28 23:36:39 +0200

    chore: switch to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
 import icon from "astro-icon";
 
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
+  output: "static",
   integrations: [icon(), playformCompress()]
 });
