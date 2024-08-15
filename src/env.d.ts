13921efb159873542d65881771828bf3ada7d251
dcschmid <daniel.schmid1980@icloud.com>
2024-08-15 20:24:39 +0200

    fix: clearLocalStorageAfterLastRound improvement

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,3 @@
+/// <reference path="../.astro/types.d.ts" />
 // src/env.d.ts
 /// <reference types="astro/client" />
--- src/utils/clearLocalStorageAfterLastRound.ts
+++ src/utils/clearLocalStorageAfterLastRound.ts
@@ -9,18 +9,21 @@ export function clearLocalStorageAfterLastRound(category: string) {
     localStorage.removeItem(`${category}-TriviaWon-round-one`);
     localStorage.removeItem(`${category}-AllCorrect-round-one`);
     localStorage.removeItem(`${category}-Results-round-one`);
+    localStorage.removeItem(`${category}-triviaData-round-one`);
 
     // Remove round two points, trivia won, all correct, and results from local storage
     localStorage.removeItem(`${category}-Points-round-two`);
     localStorage.removeItem(`${category}-TriviaWon-round-two`);
     localStorage.removeItem(`${category}-AllCorrect-round-two`);
     localStorage.removeItem(`${category}-Results-round-two`);
+    localStorage.removeItem(`${category}-triviaData-round-two`);
 
     // Remove round three points, trivia won, all correct, and results from local storage
     localStorage.removeItem(`${category}-Points-round-three`);
     localStorage.removeItem(`${category}-TriviaWon-round-three`);
     localStorage.removeItem(`${category}-AllCorrect-round-three`);
     localStorage.removeItem(`${category}-Results-round-three`);
+    localStorage.removeItem(`${category}-triviaData-round-three`);
 
     // Remove current points from local storage
     localStorage.removeItem("currentPoints");
