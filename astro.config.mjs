706a236cce7b2bd651350ddc6a9a66e15cb56637
dcschmid <daniel.schmid1980@icloud.com>
2024-08-20 21:19:13 +0200

    feat: switch to node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import playformCompress from "@playform/compress";
+import db from "@astrojs/db";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations: [icon(), playformCompress()]
+  site: "https://o9m74nbeecerwsdt12134.cleavr.xyz",
+  output: "server",
+  integrations: [icon(), playformCompress(), db()],
+  adapter: node({
+    mode: "standalone",
+  }),
+  vite: {
+    optimizeDeps: {
+      exclude: ["astro:db"],
+    },
+  },
+  security: {
+    checkOrigin: true,
+  },
 });
