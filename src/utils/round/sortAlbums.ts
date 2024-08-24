404221a6083de5f27efa7e29f9161de1a63e059a
dcschmid <daniel.schmid1980@icloud.com>
2024-08-24 15:10:15 +0200

    feat: move genre albums data in seperated json files

--- src/utils/round/sortAlbums.ts
+++ src/utils/round/sortAlbums.ts
@@ -1,7 +1,5 @@
 import type { Album } from "../interfaces/albumInterface";
-import albums from "../../json/albums.json"; // JSON file containing albums data
 import { shuffleArray } from "../../utils/share/shuffleArray";
-import { filterAlbumsByCategory } from "../../utils/share/filterAlbumsByCategroy";
 
 /**
  * Converts a sales string to a number.
@@ -45,51 +43,56 @@ const parseLength = (length: string): number => {
  * @param order The order of the sorting.
  * @returns The sorted array of albums.
  */
- function sortAlbums(albums: Album[], sortBy: string, order: string): Album[] {
-  return [...albums].sort((a, b) => {
-    let compareA: number;
-    let compareB: number;
+function sortAlbums(albums: Album[], sortBy: string, order: string): Album[] {
+  return [...albums]
+    .sort((a, b) => {
+      let compareA: number;
+      let compareB: number;
 
-    switch (sortBy) {
-      case "dataYear":
-        compareA = parseInt(a.dataYear, 10);
-        compareB = parseInt(b.dataYear, 10);
-        break;
-      case "dataSales":
-        compareA = parseSales(a.dataSales);
-        compareB = parseSales(b.dataSales);
-        break;
-      case "dataLength":
-        compareA = parseLength(a.dataLength);
-        compareB = parseLength(b.dataLength);
-        break;
-      default:
-        throw new Error(`Invalid sortBy value: ${sortBy}`);
-    }
-
-    if (compareA === compareB) {
-      return 0; // ignore equal values
-    }
-
-    if (order === "asc") {
-      return compareA - compareB;
-    } else {
-      return compareB - compareA;
-    }
-  }).filter((album, index, self) => {
-    return index === self.findIndex((a) => {
       switch (sortBy) {
         case "dataYear":
-          return a.dataYear === album.dataYear;
+          compareA = parseInt(a.dataYear, 10);
+          compareB = parseInt(b.dataYear, 10);
+          break;
         case "dataSales":
-          return a.dataSales === album.dataSales;
+          compareA = parseSales(a.dataSales);
+          compareB = parseSales(b.dataSales);
+          break;
         case "dataLength":
-          return a.dataLength === album.dataLength;
+          compareA = parseLength(a.dataLength);
+          compareB = parseLength(b.dataLength);
+          break;
         default:
           throw new Error(`Invalid sortBy value: ${sortBy}`);
       }
+
+      if (compareA === compareB) {
+        return 0; // ignore equal values
+      }
+
+      if (order === "asc") {
+        return compareA - compareB;
+      } else {
+        return compareB - compareA;
+      }
+    })
+    .filter((album, index, self) => {
+      return (
+        index ===
+        self.findIndex((a) => {
+          switch (sortBy) {
+            case "dataYear":
+              return a.dataYear === album.dataYear;
+            case "dataSales":
+              return a.dataSales === album.dataSales;
+            case "dataLength":
+              return a.dataLength === album.dataLength;
+            default:
+              throw new Error(`Invalid sortBy value: ${sortBy}`);
+          }
+        })
+      );
     });
-  });
 }
 
 /**
@@ -97,12 +100,13 @@ const parseLength = (length: string): number => {
  *
  * @param {string} sortBy - The field to sort the albums by. Can be one of "dataYear", "dataSales", or "dataLength".
  * @param {string} order - The order of the sorting (asc/desc).
- * @param {string} category - The category of albums to filter by.
+ * @param {Album[]} albums - The albums to filter by.
  * @return {Album[]} A list of sorted albums.
  */
-export function getAlbumsSortedBySpecificFieldAndCatgory(sortBy: string, order: string, category: string) {
+export async function getAlbumsSortedBySpecificFieldAndCatgory(sortBy: string, order: string, albums: Album[]) {
+
   // Filter the albums by category
-  const randomAlbums = shuffleArray(filterAlbumsByCategory(albums, category)).slice(0, 4);
+  const randomAlbums = shuffleArray(albums).slice(0, 4);
 
   // Sort the filtered albums by the specified field and order
   const sortedAlbums = sortAlbums(randomAlbums, sortBy, order);
@@ -113,6 +117,6 @@ export function getAlbumsSortedBySpecificFieldAndCatgory(sortBy: string, order: 
   return {
     randomAlbums,
     sortedAlbums,
-    solutionArray
+    solutionArray,
   };
 }
