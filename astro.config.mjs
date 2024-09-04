7db9f972c837b57db900df72668345b731015d71
dcschmid <daniel.schmid1980@icloud.com>
2024-09-04 09:04:48 +0200

    fix: set right url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cover-shuffle-ssr.onrender.com",
+  site: "https://cover-rush.de",
   output: "server",
   integrations: [icon(), db()],
   adapter: node({
